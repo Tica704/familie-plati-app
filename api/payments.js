9a1850e138ed4193299504d8f8ada5a5fbe7047a
Tica704 <pc.eventphotography@gmail.com>
2026-03-30 23:43:43 +0200

    Update payments.js

--- api/payments.js
+++ api/payments.js
@@ -4,20 +4,11 @@ export default async function handler(req, res) {
       "https://script.google.com/macros/s/AKfycbwB_LjR0Em4kyEwaAVvuzSu2SVtcRFk6uqPmgHBcMTWg-iU2aLqLRyJZvR4ifFaZmnS/exec"
     );
 
-    const text = await response.text();
+    const data = await response.json();
 
-    if (!response.ok) {
-      return res.status(response.status).json({
-        error: "Google Apps Script request failed",
-        status: response.status,
-        body: text,
-      });
-    }
-
-    res.setHeader("Content-Type", "application/json");
-    return res.status(200).send(text);
+    res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       error: "Eroare",
       message: error.message,
     });
